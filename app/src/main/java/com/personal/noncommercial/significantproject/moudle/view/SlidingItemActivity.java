67bf9ac810496a0ebb9d07a384be5f6d591487c6
lizc <1111>
2019-04-03 23:18:16 +0800

    侧滑添加

---
+++ app/src/main/java/com/personal/noncommercial/significantproject/moudle/view/SlidingItemActivity.java
@@ -0,0 +1,24 @@
+package com.personal.noncommercial.significantproject.moudle.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.personal.noncommercial.significantproject.R;
+
+
+/**
+ * @author :lizhengcao
+ * @date :2018/3/13
+ * E-mail:lizc@bsoft.com.cn
+ * @类说明
+ */
+
+public class SlidingItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_sliding);
+    }
+}
